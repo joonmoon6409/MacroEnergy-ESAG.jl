c918b8ed3f0af433f40d77da4a20cb92bc79d078
Joon Moon <jm6409@princeton.edu>
2026-06-23 11:11:38 -0400

    add NZK model

---
+++ NZK_power provincial_0623/run.jl
@@ -0,0 +1,9 @@
+using MacroEnergy
+using Gurobi
+
+(system, model) = run_case(
+    @__DIR__;
+    optimizer=Gurobi.Optimizer,
+    optimizer_attributes=("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-4),
+    lazy_load=false,
+);
