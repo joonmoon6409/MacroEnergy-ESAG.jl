e945f545993294be7c3554fa137d869891e4a5d7
Joon Moon <jm6409@princeton.edu>
2026-07-18 13:39:19 -0400

    update NZK model 0718 version

---
+++ NZK_power provincial_0718_v3_moonetal/run_v2_BNEF.jl
@@ -0,0 +1,19 @@
+using MacroEnergy
+using Gurobi
+
+# (system, model) = run_case(
+#     @__DIR__;
+#     optimizer=Gurobi.Optimizer,
+#     optimizer_attributes=("Method" => 1, "Threads" => -1),
+#     lazy_load=false,
+# );
+
+# run(`python3 $(joinpath(@__DIR__, "analyze_capacity.py"))`)
+
+(system, model) = run_case(
+    @__DIR__;
+    optimizer=Gurobi.Optimizer,
+    optimizer_attributes=("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-4, "Threads" => 1, "NumericFocus" => 1),
+    lazy_load=false,
+);
+ 
